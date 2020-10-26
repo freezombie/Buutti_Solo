374585f3f7f4a46dc8bbd42a5b34cc6526911c59
Antti Pikkuaho <antti.pikkuaho@outlook.com>
2020-10-26 15:21:46 +0200

    Error catching for basicmaths

--- basicMaths.js
+++ basicMaths.js
@@ -1,8 +1,14 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-
-console.log("SUM: " + (a + b));
-console.log("DIFFERENCE: " + (a - b));
-console.log("FRACTION: " + (a / b));
-console.log("PRODUCT: " + (a * b));
-console.log("AVERAGE: " + ((a + b) / 2));
+if(!isNaN(process.argv[2])&& !isNaN(process.argv[3]))
+{
+    console.log("SUM: " + (a + b));
+    console.log("DIFFERENCE: " + (a - b));
+    console.log("FRACTION: " + (a / b));
+    console.log("PRODUCT: " + (a * b));
+    console.log("AVERAGE: " + ((a + b) / 2));
+}
+else
+{
+    console.log("Argument(s) are not numbers or are undefined");
+} 
