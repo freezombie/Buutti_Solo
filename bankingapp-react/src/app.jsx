3878e28baf7db37832f54540eaba91a1891df065
FreeZombie <antti.pikkuaho@outlook.com>
2020-11-21 16:18:10 +0200

    after a day of fighting with material design and sass-loader, i changed to use ant design

--- bankingapp-react/src/app.jsx
+++ bankingapp-react/src/app.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import Frontpage from "./components/frontpage.jsx";
 import Users from "./components/users.jsx";
 
+
 const App = () => {
+    const loggedIn = false;
     return (
         <div>
             <Router>
                 <header>
-                    <Link to="/">Home</Link>
-                    <Link to="/users">Users</Link> 
+                    {loggedIn &&<Link to="/">Home</Link>}
+                    {loggedIn &&<Link to="/users">Users</Link>} 
                 </header>
                 <div>
                     <Switch>
                         <Route exact path="/" render={() => (
-                            <Frontpage />
+                            <Frontpage loggedIn={loggedIn}/>
                         )} />
                         <Route exact path="/users" render={() => (
                             <Users />
--- bankingapp-react/src/components/frontpage.jsx
+++ bankingapp-react/src/components/frontpage.jsx
@@ -1,9 +1,46 @@
-const Frontpage = () => {
-    return (
-    <div>
-        <p>You are on the frontpage</p>
-    </div>
-    );
+import "./frontpage.css";
+import { Button } from 'antd';
+import "antd/dist/antd.css";
+import { useState } from 'react';
+
+function Frontpage(props) {
+    const [onFrontpage, setOnFrontpage] = useState(true);
+    const [onLoginPage, setOnLoginPage] = useState(false);
+    const [onSignupPage, setOnSignupPage] = useState(false);
+
+    const activateLogin = () => {
+        console.log("going to login");
+        setOnFrontpage(false);
+        setOnLoginPage(true);
+    }
+    
+    const activateSignup = () => {
+        console.log("going to signup");
+        setOnFrontpage(false);
+        setOnSignupPage(true);
+    }
+
+    if(onFrontpage)
+    {
+        return (
+        <div id="frontPage">
+            <div id="frontPageLogo">
+                <img src="/graphic/frontpage.png" alt="frontpage logo" />
+                <Button className="leftLink" type="text" onClick={activateLogin}>Login</Button>
+                <Button className="rightLink" type="text" onClick={activateSignup}>Signup</Button>
+            </div>
+        </div>
+        );
+    } else if (onLoginPage) {
+        return (
+            <p>On login page</p>
+        );
+    } else if (onSignupPage) {
+        return (
+            <p>on SignupPage</p>
+        );
+    }
 }
 
 export default Frontpage;
+
